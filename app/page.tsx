46986b8352836b92e4ef1f78454f1059e5c7b90a
abhishekbora09 <147861205+abhishekbora09@users.noreply.github.com>
2024-09-28 17:25:16 -0400

    added app folder

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+
+import React from 'react'
+import { InteractiveChatbot } from '@/components/InteractiveChatbot'
+
+export default function Home() {
+  return (
+    <InteractiveChatbot />
+  )
+}
